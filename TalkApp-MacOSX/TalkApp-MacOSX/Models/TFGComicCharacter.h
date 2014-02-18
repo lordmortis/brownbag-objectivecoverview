c2fc33c0779563ecf6063148e17a2935259e24d9
Brendan Ragan <lordmortis@gmail.com>
2014-02-18 21:20:48 +0800

    Comic character with first name, last name and age

---
+++ TalkApp-MacOSX/TalkApp-MacOSX/Models/TFGComicCharacter.h
@@ -0,0 +1,17 @@
+//
+//  TFGComicCharacter.h
+//  TalkApp-MacOSX
+//
+//  Created by Brendan Ragan on 18/02/14.
+//  Copyright (c) 2014 The Frontier Group. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TFGComicCharacter : NSObject
+
+@property (nonatomic, strong) NSString *firstname;
+@property (nonatomic, strong) NSString *lastname;
+@property (nonatomic) int age;
+
+@end
---
+++ TalkApp-MacOSX/TalkApp-MacOSX/Models/TFGComicCharacter.m
@@ -0,0 +1,13 @@
+//
+//  TFGComicCharacter.m
+//  TalkApp-MacOSX
+//
+//  Created by Brendan Ragan on 18/02/14.
+//  Copyright (c) 2014 The Frontier Group. All rights reserved.
+//
+
+#import "TFGComicCharacter.h"
+
+@implementation TFGComicCharacter
+
+@end
