44938f8ed503f36fda35461ff153f63c1f441f97
Brendan Ragan <lordmortis@gmail.com>
2014-02-25 21:52:46 +0800

    View Controller Action!

---
+++ TalkApp-MacOSX/TalkApp-MacOSX/UI/TFGCharactersViewController.h
@@ -0,0 +1,13 @@
+//
+//  TFGCharactersViewController.h
+//  TalkApp-MacOSX
+//
+//  Created by Brendan Ragan on 25/02/14.
+//  Copyright (c) 2014 The Frontier Group. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TFGCharactersViewController : NSViewController
+
+@end
---
+++ TalkApp-MacOSX/TalkApp-MacOSX/UI/TFGCharactersViewController.m
@@ -0,0 +1,21 @@
+//
+//  TFGCharactersViewController.m
+//  TalkApp-MacOSX
+//
+//  Created by Brendan Ragan on 25/02/14.
+//  Copyright (c) 2014 The Frontier Group. All rights reserved.
+//
+
+#import "TFGCharactersViewController.h"
+
+@interface TFGCharactersViewController ()
+
+@end
+
+@implementation TFGCharactersViewController
+
+- (void)awakeFromNib {
+	NSLog(@"Hi!");
+}
+
+@end
